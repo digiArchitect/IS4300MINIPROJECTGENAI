bf2828092833aae9b6564ab0ef50bafe22209872
digiArchitect <66040569+digiArchitect@users.noreply.github.com>
2024-02-27 03:28:44 +0000

    Additional documentation

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+#These are my imports
 import google.generativeai as genai
 import os
 genai.configure(api_key=os.environ["API_KEY"])
 
 model = genai.GenerativeModel('gemini-pro')
+#Define hello_world and have the ai generate a specific sentence
 def hello_world():
   response = model.generate_content("In two sentences, give me your best response on how would you say hello to the world?")
   print(response.text)
+#Call helloworld
 hello_world();
